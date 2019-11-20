6cf8346e969e8742895b2984863b29b889358e14
dculp94 <46169494+dculp94@users.noreply.github.com>
2019-11-20 10:14:28 -0500

    Entered more sub-fields and removed master/detail

--- app/src/main/java/com/example/schemer/Strength_info.java
+++ app/src/main/java/com/example/schemer/Strength_info.java
@@ -7,11 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-import com.example.schemer.BackStrength.BackStrengthListActivity;
-import com.example.schemer.Lower_Body_Strength.LowerBackStrengthDetailActivity;
-import com.example.schemer.Lower_Body_Strength.LowerBackStrengthListActivity;
-import com.example.schemer.Upper_Body_Strength.UpperBodyStrengthListActivity;
-import com.google.android.youtube.player.YouTubeBaseActivity;
+
 
 
 public class Strength_info extends AppCompatActivity implements View.OnClickListener{
@@ -26,11 +22,11 @@ public class Strength_info extends AppCompatActivity implements View.OnClickList
 
         Upper_Body = findViewById(R.id.Upper_strength);
         Lower_Body = findViewById(R.id.Lower_strength);
-        Back_Body = findViewById(R.id.Back_strength);
+
 
         Upper_Body.setOnClickListener(this);
         Lower_Body.setOnClickListener(this);
-        Back_Body.setOnClickListener(this);
+
     }
     @Override
     public void onClick(View v) {
@@ -39,11 +35,10 @@ public class Strength_info extends AppCompatActivity implements View.OnClickList
 
         switch (v.getId()) {
             case R.id.Upper_strength:
-                i = new Intent(this, UpperBodyStrengthListActivity.class);startActivity(i); break;
+                i = new Intent(this, UpperBodyExercises.class);startActivity(i); break;
             case R.id.Lower_strength:
-                i = new Intent(this, LowerBackStrengthListActivity.class);startActivity(i); break;
-            case R.id.Back_strength:
-                i = new Intent(this, BackStrengthListActivity.class);startActivity(i); break;
+                i = new Intent(this, LowerBodyExercises.class);startActivity(i); break;
+
 
             default:break;
         }
