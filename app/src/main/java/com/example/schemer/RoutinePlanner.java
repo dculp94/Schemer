8eee06ca0c6f3b6178e2f49f457dea1926bd9b4c
dculp94 <46169494+dculp94@users.noreply.github.com>
2020-01-26 19:28:30 -0500

    Edited Calendar display features

--- app/src/main/java/com/example/schemer/RoutinePlanner.java
+++ app/src/main/java/com/example/schemer/RoutinePlanner.java
@@ -1,8 +1,14 @@
 package com.example.schemer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class RoutinePlanner extends AppCompatActivity {
 
@@ -10,5 +16,14 @@ public class RoutinePlanner extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_routine_planner);
+        CalendarView calendarView = findViewById(R.id.calendarView);
+        final TextView selectedDay = findViewById(R.id.selectedDay);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
+                selectedDay.setText("Selected Day: "+ i2);
+            }
+        });
+
     }
 }
