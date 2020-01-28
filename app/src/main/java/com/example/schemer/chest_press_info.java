3a45ae883fcccfd5f4126668047e2d1cba06b853
dculp94 <46169494+dculp94@users.noreply.github.com>
2020-01-28 07:50:46 -0500

    Fixed some xml settings in chest press to configure template and also adding add to Calendar Button

--- app/src/main/java/com/example/schemer/chest_press_info.java
+++ app/src/main/java/com/example/schemer/chest_press_info.java
@@ -1,32 +1,68 @@
 package com.example.schemer;
 
+import android.app.DatePickerDialog;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.DialogFragment;
 
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.Calendar;
 
 public class chest_press_info extends AppCompatActivity {
 
+Button dateSelection;
+TextView DateView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chest_press_info);
+
+        dateSelection = findViewById(R.id.dateSelection);
+        dateSelection.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //DialogFragment datePicker = new DatePickerFragment();
+               // datePicker.show(getSupportFragmentManager(), "Date picker");
+            }
+        });
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "This button will open up datepicker to add to Calendar", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    }
+
+            FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+            fab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }
+            });
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth)
+        {
+            Calendar c = Calendar.getInstance();
+            c.set(Calendar.YEAR,year);
+            c.set(Calendar.MONTH,month);
+            c.set(Calendar.DAY_OF_MONTH,dayOfMonth);
+            String currentDateString = DateFormat.getDateInstance(DateFormat.FULL).format(c.getTime());
+            DateView = findViewById(R.id.selectionView);
+            DateView.setText(currentDateString);
+        }
 }
+
+
