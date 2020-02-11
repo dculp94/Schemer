dab9de166b6de4f6340ee992ec1cf7e206d7c2dc
dculp94 <46169494+dculp94@users.noreply.github.com>
2020-02-11 00:06:16 -0500

    Cleaned up chest press UI and worked on Calendar Dialog setup

--- app/src/main/java/com/example/schemer/RoutinePlanner.java
+++ app/src/main/java/com/example/schemer/RoutinePlanner.java
@@ -8,22 +8,28 @@ import android.widget.CalendarView;
 import android.widget.DatePicker;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 import java.util.Calendar;
 
 public class RoutinePlanner extends AppCompatActivity {
+    TextView AddedExercise;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_routine_planner);
         CalendarView calendarView = findViewById(R.id.calendarView);
         final TextView selectedDay = findViewById(R.id.selectedDay);
+
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
                 selectedDay.setText("Selected Day: "+ i2);
             }
         });
+
 
     }
 }
--- app/src/main/java/com/example/schemer/chest_press_info.java
+++ app/src/main/java/com/example/schemer/chest_press_info.java
@@ -2,7 +2,10 @@ package com.example.schemer;
 
 import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
+import android.nfc.Tag;
 import android.os.Build;
 import android.os.Bundle;
 
@@ -14,32 +17,47 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.DialogFragment;
 
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.CalendarView;
 import android.widget.DatePicker;
 import android.widget.TextView;
 
 import java.text.DateFormat;
 import java.util.Calendar;
 
-public class chest_press_info extends AppCompatActivity {
+public class chest_press_info extends AppCompatActivity implements DatePickerDialog.OnDateSetListener{
 
+private static final String TAG = "chest_press_info";
+
+TextView dateText;
 Button dateSelection;
-TextView DateView;
 private WebView webView;
+private DatePickerDialog.OnDateSetListener mDateSetListener;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chest_press_info);
 
         // URL display on information
-        webView = (WebView) findViewById(R.id.webView_chestPress);
+        webView = findViewById(R.id.webView_chestPress);
         webView.setWebViewClient(new WebViewClient());
         webView.loadUrl("https://www.wikihow.com/Bench-Press");
-        webView.setVerticalScrollBarEnabled(true);
+
+        dateSelection = findViewById(R.id.dateSelection);
+        dateSelection.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               showDatePickerDialog();
+            }
+        });
 
         webView.setWebViewClient(new WebViewClient() {
             @Override
@@ -49,32 +67,32 @@ private WebView webView;
             }
         });
 
-        WebSettings webSettings = webView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-
-        dateSelection = findViewById(R.id.dateSelection);
-        dateSelection.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //DialogFragment datePicker = new DatePickerFragment();
-               // datePicker.show(getSupportFragmentManager(), "Date picker");
-            }
-        });
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        dateText = findViewById(R.id.dateText);
 
 
-            FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-            fab.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                            .setAction("Action", null).show();
-                }
-            });
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        }
+
+
+    }
+
+
+    private void showDatePickerDialog()
+    {
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+                this,
+                this,
+                Calendar.getInstance().get(Calendar.YEAR),
+                Calendar.getInstance().get(Calendar.MONTH),
+                Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
+        );
+        datePickerDialog.show();
+
+    }
+
+    @Override
+    public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
+        String date = "month/day/year: " + month + "/" + dayOfMonth + "/" + year;
+        dateText.setText(date);
+    }
 
         @Override
         public void onBackPressed()
@@ -89,21 +107,16 @@ private WebView webView;
             }
 
         }
+
+
+
     public void openChestVideo(View view) {
         Intent browserIntent = new Intent(Intent.ACTION_VIEW,
                 Uri.parse("https://www.youtube.com/watch?v=SCVCLChPQFY"));
         startActivity(browserIntent);
     }
-        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth)
-        {
-            Calendar c = Calendar.getInstance();
-            c.set(Calendar.YEAR,year);
-            c.set(Calendar.MONTH,month);
-            c.set(Calendar.DAY_OF_MONTH,dayOfMonth);
-            String currentDateString = DateFormat.getDateInstance(DateFormat.FULL).format(c.getTime());
-            DateView = findViewById(R.id.selectionView);
-            DateView.setText(currentDateString);
-        }
+
+
 }
 
 
